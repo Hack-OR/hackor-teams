2d82703467d6691232fb4aad3d2cc89784b52f81
Aaron Esau <git@aaronesau.com>
2021-01-06 20:48:18 -0800

    work on getting reactions working

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 
 import discord
-from discord.ext import commands
+import discord.ext.commands
 
 import yaml
 import logging
 
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.reactions = True
+client = discord.ext.commands.Bot(command_prefix='!', intents=intents)
 
 config = dict()
+msg_settings = {'allowed_mentions': discord.AllowedMentions(everyone=False, roles=set())}
 
 
 ##########
 
 
 _user_cache = dict()
-def resolve_user(ctx, user: str, use_cache: bool=False) -> discord.Member:
+async def resolve_user(ctx, user: str, use_cache: bool=False) -> discord.Member:
     assert isinstance(user, str)
 
-    converter = MemberConverter() # XXX: should I move this to the global scope?
+    converter = discord.ext.commands.MemberConverter() # XXX: should I move this to the global scope?
     member = _user_cache.get(user)
     if (not use_cache) or not member:
-        member = await converter.convert(ctx, user)
-        _user_cache[user] = member
+        try:
+            member = await converter.convert(ctx, user)
+            _user_cache[user] = member
+        except discord.ext.commands.errors.MemberNotFound:
+            member = None
 
     return member
 
@@ -38,20 +43,26 @@ async def on_ready():
     #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='to your requests'))
 
 
-@client.event
-async def on_reaction_add(reaction, member):
-    if reaction.message.id == config['specializations']['message-id']:
-        name = reaction.emoji.name
+@discord.ext.commands.Cog.listener()
+async def on_raw_reaction_add(self, payload):
+    print(message.id, config['specializations']['message-id'])
+    if payload.message_id == config['specializations']['message-id']:
+        channel = await self.bot.fetch_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await self.bot.fetch_user(payload.user_id)
+        emoji = payload.emoji
+
+        name = emoji.name
         try:
             tag = config['specializations']['emojis'].get(name)
             if not tag:
-                reaction.message.remove_reaction(reaction.emoji, member)
+                message.remove_reaction(emoji, member)
                 assert tag, f'Emoji "{name}" is meaningless'
         except Exception as e:
             logging.error('when adding reaction: ' + str(e))
 
 
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def request(ctx, *args):
     # don't actually do anything with the users we find here;
     # we do all of the hard work once the event starts. For now, just check to 
@@ -59,17 +70,27 @@ async def request(ctx, *args):
     _users_not_found = list()
     for username in set(args):
         # the work usually won't be wasted since it's stored in cache
-        if not resolve_user(ctx, username):
+        if not (user := await resolve_user(ctx, username)):
             _users_not_found.append(username)
+        else:
+            # check to make sure the user has the right role
+            pass
 
     if _users_not_found:
-        # TODO: send message saying the users weren't found. don't forget to
-        # sanitize the usernames
-        pass
+        # send message saying the users weren't found. 
+        # TODO: don't forget to sanitize the usernames
+        msg = '**Warning:** Unable to find users: `' + '`, `'.join(_users_not_found) + '`'
+        await ctx.send(msg, **msg_settings)
     else:
         # TODO: add a check mark reaction to the message. search for previous
         # commands, and drop the old check mark if exists.
-        pass
+        await ctx.message.add_reaction('white_check_mark')
+
+
+@client.command(pass_context=True)
+async def ping(ctx):
+    await ctx.send('Pong @everyone', **msg_settings)
+
 
 ##########
 
